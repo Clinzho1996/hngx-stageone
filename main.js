f79c67ba190b04ed2bff75fe30a0df365f093d10
Clinzho1996 <confidinho@yahoo.com>
2023-09-08 22:39:28 +0100

    day fixed

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+const currentDate = new Date();
+
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const currentDay = daysOfWeek[currentDate.getUTCDay()];
+
+const currentDayElement = document.getElementById("currentDay");
+currentDayElement.innerHTML = `${currentDay}`;
+
 const desiredTime = new Date();
 desiredTime.setUTCHours(8);
 desiredTime.setUTCMinutes(15);
